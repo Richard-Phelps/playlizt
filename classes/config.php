7844f89cfd078f20177c1422148b641c72a15fe5
Richard Phelps <richard.phelps123@hotmail.com>
2017-07-21 22:55:56 +0100

    IC

---
+++ classes/config.php
@@ -0,0 +1,36 @@
+<?php
+    class config
+    {
+
+        /**
+         * Constructor
+         */
+        public function __construct ()
+        {
+
+            $this->site_url = 'http://192.168.33.10/personal-playlizt';
+            $this->debug    = true;
+            $this->db_host  = 'localhost';
+            $this->db_user  = 'root';
+            $this->db_pass  = 'root';
+            $this->db_name  = 'personal_playlizt';
+
+            $this->error_reporting();
+
+        }
+
+        /**
+         * Method to deal with error reporting
+         */
+        public function error_reporting ()
+        {
+
+            if ($debug) {
+                ini_set('display_errors', 'on');
+                error_reporting(E_ALL);
+            }
+
+        }
+
+    }
+?>
