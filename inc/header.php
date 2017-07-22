b74f60198457b557e3d3ecaaef1b5120b9777231
Richard Phelps <richard.phelps123@hotmail.com>
2017-07-22 15:03:54 +0100

    Setting up the design for the site and the homepage. Also setting up the htaccess and adding security and changing the url to remove the php extension and add a trailing slash

--- inc/header.php
+++ inc/header.php
@@ -2,8 +2,36 @@
 <html>
 
 <head>
+
+    <!-- Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Stylesheets -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300">
     <link rel="stylesheet" href="<?php echo $config->site_url; ?>/css/styles.css">
+
 </head>
 
-<body>
+<body class="light-grey-bg">
+
+<header class="orange-bg white-text smooth-box-shadow">
+
+    <div class="container">
+
+        <div class="row mb0">
+
+            <div class="col m6 left-align">
+                <p class="margin0 header-text-size header-line-height">Playlizt</p>
+            </div>
+
+            <div class="col m6 right-align">
+                <i class="material-icons pointer-cursor header-line-height">account_circle</i>
+            </div>
+
+        </div><!-- .row -->
+
+    </div><!-- .container -->
+
+</header>
--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 <?php require_once('inc/header.php'); ?>
 
+<div class="page-wrapper">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="input-field col s12">
+                <input id="playlist_name" type="text">
+                <label for="playlist_name" class="orange-text-focus-important">Name Your Playlist</label>
+            </div>
+
+        </div>
+
+    </div><!-- .container -->
+
+</div><!-- .page-wrapper -->
+
 <?php require_once('inc/footer.php'); ?>
